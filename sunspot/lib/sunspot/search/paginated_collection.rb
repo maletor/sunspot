a5a0472f7ad7817a4ad7340c4b61df98f5d94fd0
Ellis Berner <eberner@gmail.com>
2012-01-10 12:12:24 -0800

    add respond_to?

--- sunspot/lib/sunspot/search/paginated_collection.rb
+++ sunspot/lib/sunspot/search/paginated_collection.rb
@@ -2,11 +2,9 @@ module Sunspot
   module Search
 
     class PaginatedCollection
-      instance_methods.each { |m| undef_method m unless m =~ /^__|instance_eval|object_id/ }
-
-      attr_reader :total_count, :current_page, :per_page
-      alias :total_entries :total_count
-      alias :limit_value :per_page
+      (instance_methods-%w[instance_eval object_id respond_to? class inspect]).each { |m|
+        undef_method m unless m =~ /^__|instance_eval|object_id/
+      }
 
       def initialize(collection, page, per_page, total)
         @collection   = collection
@@ -15,33 +13,9 @@ module Sunspot
         @total_count  = total
       end
 
-      def total_pages
-        (total_count.to_f / per_page).ceil
-      end
-      alias :num_pages :total_pages
 
-      def first_page?
-        current_page == 1
-      end
-
-      def last_page?
-        current_page >= total_pages
-      end
-
-      def previous_page
-        current_page > 1 ? (current_page - 1) : nil
-      end
-
-      def next_page
-        current_page < total_pages ? (current_page + 1) : nil
-      end  
-
-      def out_of_bounds?
-        current_page > total_pages
-      end
-      
-      def offset
-        (current_page - 1) * per_page
+      def respond_to?(*args)
+        super || @collection.respond_to?(*args)
       end
 
       private
@@ -49,7 +23,7 @@ module Sunspot
       def method_missing(method, *args, &block)
         @collection.send(method, *args, &block)
       end
-
     end
+
   end
 end
